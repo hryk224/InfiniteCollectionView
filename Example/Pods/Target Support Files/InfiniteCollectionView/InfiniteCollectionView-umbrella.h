6f73cf41cbda0a98d00a1e7f8a1a7c6aa3342610
hyyk224 <hryk224@gmail.com>
2016-05-11 04:41:27 +0900

    Updated example

--- Example/Pods/Target Support Files/InfiniteCollectionView/InfiniteCollectionView-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double InfiniteCollectionViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char InfiniteCollectionViewVersionString[];
-
---
+++ InfiniteCollectionView/InfiniteCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteCollectionView.h
+//  InfiniteCollectionView
+//
+//  Created by hiroyuki yoshida on 2016/05/11.
+//  Copyright © 2016年 hiroyuki yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfiniteCollectionView.
+FOUNDATION_EXPORT double InfiniteCollectionViewVersionNumber;
+
+//! Project version string for InfiniteCollectionView.
+FOUNDATION_EXPORT const unsigned char InfiniteCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteCollectionView/PublicHeader.h>
+
+
