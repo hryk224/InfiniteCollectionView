a9767f3c936472d04c8ae42aba2513b80d86f005
Aron Pavel <ext.aron.pavel@sats.no>
2021-09-16 00:20:44 +0200

    Updated package description

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 
 let package = Package(
     name: "InfiniteCollectionView",
-    targets: [],
     dependencies: [
-        .Package(url: "https://github.com/hryk224/InfiniteCollectionView", majorVersion: 1),
-        ]
+        .package(url: "https://github.com/pavelaron/InfiniteCollectionView", from: "1.3.2"),
+    ],
+    targets: []
 )
