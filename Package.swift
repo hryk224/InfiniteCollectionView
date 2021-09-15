37277ed8a68fe23f5fb9a124b2c768f6df0c16ee
Aron Pavel <ext.aron.pavel@sats.no>
2021-09-16 00:32:21 +0200

    Added product to package description

--- Package.swift
+++ Package.swift
@@ -3,8 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "InfiniteCollectionView",
+    products: [
+        .library(
+            name: "InfiniteCollectionView",
+            targets: ["InfiniteCollectionView"]
+        )
+    ],
     dependencies: [
-        .package(url: "https://github.com/pavelaron/InfiniteCollectionView", from: "1.3.3"),
+        .package(url: "https://github.com/pavelaron/InfiniteCollectionView", from: "1.3.4"),
     ],
     targets: []
 )
