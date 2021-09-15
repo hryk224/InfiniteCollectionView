9d653c9bafecac7d5d49311e38e632161d32f05e
Aron Pavel <ext.aron.pavel@sats.no>
2021-09-16 01:05:04 +0200

    Fixed missing @objc from method

--- Sources/InfiniteCollectionView.swift
+++ Sources/InfiniteCollectionView.swift
@@ -46,7 +46,7 @@ open class InfiniteCollectionView: UICollectionView {
     deinit {
         NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
     }
-    open func rotate(_ notification: Notification) {
+    @objc open func rotate(_ notification: Notification) {
         setContentOffset(CGPoint(x: CGFloat(pageIndex + indexOffset) * itemWidth, y: contentOffset.y), animated: false)
     }
     open override func selectItem(at indexPath: IndexPath?, animated: Bool, scrollPosition: UICollectionView.ScrollPosition) {
